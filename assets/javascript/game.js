a131b420e520ae77f82f88ddd79cc56abdaf042c
91integ25 <91integ25@gmail.com>
2017-02-18 02:34:48 -0500

    finished psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,61 @@
+
+
+
+var compGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","z"];
+var lettersGuessed = [];
+var guessesLeft = 8;
+var	winCount = 1;
+var loseCount = 1;
+
+
+function lose() {
+
+			if (guessesLeft <= 0 ){
+
+				guessesLeft = 9;
+
+				lettersGuessed = [];
+
+				document.getElementById("losses").innerHTML = loseCount++;
+				console.log("you lose")
+
+			}
+			
+		}
+		
+		function win() {
+			console.log("You win!!")
+    			
+    			document.getElementById('wins').innerHTML = winCount++;
+    			lettersGuessed = [];
+    			guessesLeft = 8;
+		}
+		
+		function tryAgain() {
+				
+    			console.log("try again")
+				
+				document.getElementById("guessesLeft").innerHTML = guessesLeft--;
+
+				lettersGuessed.push(event.key);
+				
+				document.getElementById('guessedLetters').innerHTML = lettersGuessed;
+		}
+ 
+    
+    document.onkeyup = function () {
+		var	randomLetter = compGuess[Math.floor(Math.random() * compGuess.length)];
+    		var userGuess = event.key;
+    		lose();	
+    		if (userGuess.indexOf(randomLetter) === -1){
+    			tryAgain();
+			}
+			else {
+
+				win();
+			}
+    			
+	}
+
+
+
